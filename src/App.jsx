5b5fb664b0ac0f43ee7bd611c2ecf021497c7b99
Ivan Radko <mr.radkoivan@gmail.com>
2023-08-29 11:54:36 +0300

    header_desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import "./App.css";
+import "../node_modules/modern-normalize/modern-normalize.css";
+import "./reset.css";
+import Header from "./components/header/Header";
+import Hero from "./components/hero/Hero";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [scroll, setScroll] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 50);
+      console.log("Scroll");
+    });
+  }, []);
+
   return (
     <>
-      <h1>Test Netlify</h1>
+      <header>
+        <Header className={scroll ? "header stickyadd" : "header"} />
+      </header>
+      <main>
+        <Hero />
+      </main>
+      <footer></footer>
     </>
   );
 }
