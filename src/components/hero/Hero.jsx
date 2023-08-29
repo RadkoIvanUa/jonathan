807fa65e7c8f24432b91e3b6df64f186bd74f018
Ivan Radko <mr.radkoivan@gmail.com>
2023-08-29 18:08:30 +0300

    added responsive hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,9 +1,36 @@
+import { useRef } from "react";
 import "./hero.css";
 
+import { SlMouse } from "react-icons/sl";
+
 export default function Hero() {
+  const windowSize = useRef([window.innerWidth]);
+
   return (
     <>
-      <div className="hero"></div>
+      <div className="hero">
+        <div className="container">
+          <p className="hero_text">Welcome</p>
+          <h1 className="hero_title">I`m a Graphic designer</h1>
+          <p className="hero_subtitle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit, nostrum!
+          </p>
+          <button className="hero_btn">
+            <a
+              href="public\download_sample\download_sample.txt"
+              download={true}
+            >
+              Download CV
+            </a>
+          </button>
+          <button className="hero_scroll" type="button">
+            <SlMouse
+              size={windowSize.current[0] < 768 ? 40 : 60}
+              color="white"
+            />
+          </button>
+        </div>
+      </div>
     </>
   );
 }
