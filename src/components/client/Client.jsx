879e22294f47127289ed7bcc384c9ad350226074
Ivan Radko <mr.radkoivan@gmail.com>
2023-10-13 17:34:50 +0300

    clients array

--- src/components/client/Client.jsx
+++ src/components/client/Client.jsx
@@ -5,12 +5,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper/modules";
 import { RiDoubleQuotesL, RiDoubleQuotesR } from "react-icons/ri";
 import { motion } from "framer-motion";
+import clientsList from "./clients_list";
 import PropTypes from "prop-types";
-import client1 from "../../img/client/client_1.jpg";
-import client2 from "../../img/client/client_2.jpg";
-import client3 from "../../img/client/client_3.jpg";
-import client4 from "../../img/client/client_4.jpg";
-import client5 from "../../img/client/client_5.jpg";
 
 const swiperSlideStyle = {
   padding: 20,
@@ -66,169 +62,42 @@ export default function Client({ selectedColor }) {
                 "--swiper-pagination-color": `${selectedColor}`,
               }}
             >
-              <SwiperSlide
-                style={swiperSlideStyle}
-                className="client__swiper-slide"
-              >
-                <div className="client__slide-top">
-                  <div className="client__slide-thumb">
-                    <img src={client1} className="client__slide-img" />
-                  </div>
-                  <div>
-                    <h3 className="client__slide-title">Johny Black</h3>
-                    <h4 className="client__slide-subtitle">
-                      FronEnd developer
-                    </h4>
-                  </div>
-                </div>
+              <ul>
+                {clientsList.map((client, index) => (
+                  <li key={index}>
+                    <SwiperSlide
+                      style={swiperSlideStyle}
+                      className="client__swiper-slide"
+                    >
+                      <div className="client__slide-top">
+                        <div className="client__slide-thumb">
+                          <img src={client.img} className="client__slide-img" />
+                        </div>
+                        <div>
+                          <h3 className="client__slide-title">{client.name}</h3>
+                          <h4 className="client__slide-subtitle">
+                            {client.role}
+                          </h4>
+                        </div>
+                      </div>
 
-                <p className="client__slide-testimonial">
-                  <RiDoubleQuotesL
-                    size={20}
-                    className="quotes__start"
-                    color={selectedColor}
-                  />{" "}
-                  I stumbled upon this service, and I must say, it's a true gem!
-                  The innovative approach they take to simplify complex concepts
-                  and ideas is simply remarkable. It's like they have a magical
-                  way of distilling complexity into elegant simplicity. The user
-                  interface is clean and intuitive, making the entire experience
-                  a breeze.
-                  <RiDoubleQuotesR
-                    size={20}
-                    className="quotes__end"
-                    color={selectedColor}
-                  />
-                </p>
-              </SwiperSlide>
-              <SwiperSlide
-                style={swiperSlideStyle}
-                className="client__swiper-slide"
-              >
-                <div className="client__slide-top">
-                  <div className="client__slide-thumb">
-                    <img src={client2} className="client__slide-img" />
-                  </div>
-                  <div>
-                    <h3 className="client__slide-title">Monica Layola</h3>
-                    <h4 className="client__slide-subtitle">Project Manager</h4>
-                  </div>
-                </div>
-
-                <p className="client__slide-testimonial">
-                  <RiDoubleQuotesL
-                    size={20}
-                    className="quotes__start"
-                    color={selectedColor}
-                  />
-                  It's like a portal to a world of creative thinking and fresh
-                  ideas. Using it feels like a journey into the unknown, with
-                  each abstract concept unveiling a new layer of intrigue. The
-                  way it seamlessly blends simplicity and depth is
-                  mind-boggling.
-                  <RiDoubleQuotesR
-                    size={20}
-                    className="quotes__end"
-                    color={selectedColor}
-                  />
-                </p>
-              </SwiperSlide>
-              <SwiperSlide
-                style={swiperSlideStyle}
-                className="client__swiper-slide"
-              >
-                <div className="client__slide-top">
-                  <div className="client__slide-thumb">
-                    <img src={client3} className="client__slide-img" />
-                  </div>
-                  <div>
-                    <h3 className="client__slide-title">Antonio White</h3>
-                    <h4 className="client__slide-subtitle">Desinger</h4>
-                  </div>
-                </div>
-
-                <p className="client__slide-testimonial">
-                  <RiDoubleQuotesL
-                    size={20}
-                    className="quotes__start"
-                    color={selectedColor}
-                  />
-                  Discovering this service has been a delightful surprise. It's
-                  like having access to a treasure trove of creative concepts.
-                  The user-friendly interface makes exploring abstract ideas a
-                  breeze, and it's been an invaluable tool for my work. I've
-                  found it to be a source of inspiration and a catalyst for
-                  innovative thinking.
-                  <RiDoubleQuotesR
-                    size={20}
-                    className="quotes__end"
-                    color={selectedColor}
-                  />
-                </p>
-              </SwiperSlide>
-              <SwiperSlide
-                style={swiperSlideStyle}
-                className="client__swiper-slide"
-              >
-                <div className="client__slide-top">
-                  <div className="client__slide-thumb">
-                    <img src={client4} className="client__slide-img" />
-                  </div>
-                  <div>
-                    <h3 className="client__slide-title">Jesica Parcker</h3>
-                    <h4 className="client__slide-subtitle">Graphic Desinger</h4>
-                  </div>
-                </div>
-
-                <p className="client__slide-testimonial">
-                  <RiDoubleQuotesL
-                    size={20}
-                    className="quotes__start"
-                    color={selectedColor}
-                  />
-                  I recently started using this service, and it's been a
-                  game-changer for my creative projects. It's as if they've
-                  harnessed the power of imagination and distilled it into a
-                  digital tool. Navigating through their abstract concepts is
-                  like embarking on a fascinating intellectual journey.
-                  <RiDoubleQuotesR
-                    size={20}
-                    className="quotes__end"
-                    color={selectedColor}
-                  />
-                </p>
-              </SwiperSlide>
-              <SwiperSlide
-                style={swiperSlideStyle}
-                className="client__swiper-slide"
-              >
-                <div className="client__slide-top">
-                  <div className="client__slide-thumb">
-                    <img src={client5} className="client__slide-img" />
-                  </div>
-                  <div>
-                    <h3 className="client__slide-title">Donald Rich</h3>
-                    <h4 className="client__slide-subtitle">Photographer</h4>
-                  </div>
-                </div>
-
-                <p className="client__slide-testimonial">
-                  <RiDoubleQuotesL
-                    size={20}
-                    className="quotes__start"
-                    color={selectedColor}
-                  />
-                  This service is nothing short of mind-blowing. It's like a
-                  portal to a world of endless possibilities. The way it breaks
-                  down complex ideas into bite-sized, understandable pieces is
-                  genius.
-                  <RiDoubleQuotesR
-                    size={20}
-                    className="quotes__end"
-                    color={selectedColor}
-                  />
-                </p>
-              </SwiperSlide>
+                      <p className="client__slide-testimonial">
+                        <RiDoubleQuotesL
+                          size={20}
+                          className="quotes__start"
+                          color={selectedColor}
+                        />{" "}
+                        {client.response}
+                        <RiDoubleQuotesR
+                          size={20}
+                          className="quotes__end"
+                          color={selectedColor}
+                        />
+                      </p>
+                    </SwiperSlide>
+                  </li>
+                ))}
+              </ul>
             </Swiper>
           </motion.div>
         </div>
---
+++ src/components/client/clients_list.js
@@ -0,0 +1,45 @@
+import one from "../../img/client/client_1.jpg";
+import two from "../../img/client/client_2.jpg";
+import three from "../../img/client/client_3.jpg";
+import four from "../../img/client/client_4.jpg";
+import five from "../../img/client/client_5.jpg";
+
+const clientsList = [
+  {
+    img: one,
+    name: "Johny Black",
+    role: "FronEnd developer",
+    response:
+      "I stumbled upon this service, and I must say, it's a true gem! The innovative approach they take to simplify complex concepts and ideas is simply remarkable. It's like they have a magical way of distilling complexity into elegant simplicity. The user interface is clean and intuitive, making the entire experience a breeze.",
+  },
+  {
+    img: two,
+    name: "Monica Simons",
+    role: "Web Desinger",
+    response:
+      "I chanced upon this offering, and I'm genuinely impressed! Their creative strategy for simplifying intricate concepts and notions is truly awe-inspiring. It's as if they possess a mystical knack for converting complexity into elegant simplicity. The user interface is sleek and user-friendly, making the entire experience a walk in the park.",
+  },
+  {
+    img: three,
+    name: "Tonny White",
+    role: "Freelancer",
+    response:
+      "This service caught my eye, and I can't help but praise it! Their unique method of demystifying complex ideas and theories is truly exceptional. It's like they wield a magical wand, turning complexity into graceful simplicity. The user interface is polished and intuitive, ensuring a smooth experience throughout.",
+  },
+  {
+    img: four,
+    name: "Linda Simpson",
+    role: "HR ",
+    response:
+      "I happened upon this service, and it's truly outstanding! Their fresh approach to simplifying intricate concepts and ideas is incredibly impressive. It's as though they possess a magical ability to distill complexity into elegant simplicity. The user interface is clean and intuitive, providing a seamless experience.    ",
+  },
+  {
+    img: five,
+    name: "Dilon Madison",
+    role: "Photographer",
+    response:
+      "I stumbled across this service, and it's a hidden treasure! Their ingenious way of breaking down complex concepts and ideas is absolutely remarkable. It's like they have a special gift for turning complexity into graceful simplicity. The user interface is sleek and easy to navigate, making the whole experience effortless.",
+  },
+];
+
+export default clientsList;
