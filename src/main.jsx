7b18e1b009aad915f3850e269f0c30c1d2c8a549
Ivan Radko <mr.radkoivan@gmail.com>
2023-10-16 18:32:24 +0300

    final

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { AnimatePresence, motion } from "framer-motion";
-import CustomCursor from "./components/cursor/Cursor.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
